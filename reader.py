42fe1b04252d5f43541671a9b74191552934bac2
John Victor <johnvictorio3@gmail.com>
2019-10-01 18:12:35 -0300

    WIP: reader.py

--- reader.py
+++ reader.py
@@ -3,8 +3,16 @@ import csv,json
 with open('citys.json', encoding='utf-8') as arq:
     citys = json.load(arq)
 
+
 with open('citys.csv', 'w') as arq_csv:
-    fieldnames=['name', 'voters']
+    fieldnames=['name','voters', 'url', 'votos_brancos', 'votos_nulos', 'vagas_vereadores']
     writer = csv.DictWriter(arq_csv, fieldnames=fieldnames)
     writer.writeheader()
-    writer.writerows(citys)
+
+    for x,y in citys.items():
+        print(x,y)
+        row = dict()
+        row['name'] = x
+
+        writer.writerow()
+    #writer.writerows(citys)
